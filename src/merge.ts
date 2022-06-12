320796e61186ad2e1b7855da4b68bb87a5e72fe0
Krissy510 <56779304+Krissy510@users.noreply.github.com>
2022-06-12 19:25:10 +0700

    Update new logic

--- src/merge.ts
+++ src/merge.ts
@@ -6,35 +6,33 @@ export function merge(collection_1:number[], collection_2:number[]): number[]{
     var length_2:number = collection_2.length;
 
     while(index_1 < length_1 || index_2 < length_2){
-        if (!(index_1 < length_1)){ // If index_1 already exceed or equal to length_1
-            var item_2 = collection_2[index_2];
-            combined.push(item_2);
-            index_2++;
-        }
-        else if(!(index_2 < length_2)){ // If index_2 already exceed or equal to length_2
-            var item_1 = collection_2[index_1];
-            combined.push(item_1);
-            index_1++;
-        }
-        else{ // If both index did not reach their length yet
+        var item_1 = collection_1[index_1];
+        var item_2 = collection_2[index_2];
 
-            var item_1 = collection_1[index_1];
-            var item_2 = collection_2[index_2];
-
-            if(item_1 > item_2){ // If item_2 is less than item_1
-                combined.push(item_2);
-                index_2++;
-            }
-            else{   // If item_1 is less than or equal to item_2
+        if (item_1 !== undefined) {
+            if (item_2 === undefined) {
                 combined.push(item_1);
                 index_1++;
+            } else {
+                if (item_1 <= item_2) {
+                    combined.push(item_1);
+                    index_1++;
+                } else {
+                    combined.push(item_2);
+                    index_2++;
+                }
+            }
+        } else {
+            if (item_2 !== undefined) {
+                combined.push(item_2);
+                index_2++;
             }
         }
     }
     return combined;
 }
 
-var col_1: number[] = [];
-var col_2: number[] = [];
+var col_1: number[] = [4,5,6,9];
+var col_2: number[] = [1,2,3,7];
 
 console.log(merge(col_1,col_2))
