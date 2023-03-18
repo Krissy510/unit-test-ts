e40b2a7ebad12e067b26c82f15d1be918656933b
Kris Raviphas <56779304+Krissy510@users.noreply.github.com>
2023-03-18 10:46:37 +0000

    Optimize code

--- src/merge.ts
+++ src/merge.ts
@@ -1,38 +1,27 @@
-export function merge(collection_1:number[], collection_2:number[]): number[]{
-    var combined: number[] = [];
-    var index_1: number = 0;
-    var index_2:number = 0;
-    var length_1:number = collection_1.length;
-    var length_2:number = collection_2.length;
+export function merge(collection_1: readonly number[], collection_2: readonly number[]): number[] {
+    const combined: number[] = [];
+    let index_1 = 0;
+    let index_2 = 0;
+    const length_1 = collection_1.length;
+    const length_2 = collection_2.length;
 
-    while(index_1 < length_1 || index_2 < length_2){
-        var item_1 = collection_1[index_1];
-        var item_2 = collection_2[index_2];
+    for (let i = 0; i < length_1 + length_2; i++) {
+        const item_1 = collection_1[index_1];
+        const item_2 = collection_2[index_2];
 
-        if (item_1 !== undefined) {
-            if (item_2 === undefined) {
-                combined.push(item_1);
-                index_1++;
-            } else {
-                if (item_1 <= item_2) {
-                    combined.push(item_1);
-                    index_1++;
-                } else {
-                    combined.push(item_2);
-                    index_2++;
-                }
-            }
-        } else {
-            if (item_2 !== undefined) {
-                combined.push(item_2);
-                index_2++;
-            }
+        if (item_1 !== undefined && (item_2 === undefined || item_1 <= item_2)) {
+            combined.push(item_1);
+            index_1++;
+        } else if (item_2 !== undefined) {
+            combined.push(item_2);
+            index_2++;
         }
     }
+
     return combined;
 }
 
-var col_1: number[] = [4,5,6,9];
-var col_2: number[] = [1,2,3,7];
+const col_1: readonly number[] = [4,5,6,9];
+const col_2: readonly number[] = [1,2,3,7];
 
-console.log(merge(col_1,col_2))
+console.log(merge(col_1, col_2));
